8a213c3d2690971214cdb191f67d8583e99f7fba
20397986862 <ncarratala@indracompany.com>
2021-02-16 18:12:58 -0300

    reducer

--- src/redux/reducers/tasksReducer.js
+++ src/redux/reducers/tasksReducer.js
@@ -1,9 +1,13 @@
 var intialState = {
-    tareas:['nada']
+    tasks:['nada']
 }
 
 const TasksReducer = ( state = intialState, action) => {
-
+    switch( action.type ){
+        case "ADD_TASK":
+            return tasks 
+        case "REMOVE_TASK"
+    }
 }
 
 export default TasksReducer;
